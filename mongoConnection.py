f90df4322b205778c3c399bf46defa954dc92a16
Fabien Herry <fabien_herry@yahoo.fr>
2025-05-06 15:59:32 +0200

    Passage en machine distante

--- mongoConnection.py
+++ mongoConnection.py
@@ -2,16 +2,17 @@ import pymongo
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
 def GetConnection():
-    path = "mongodb://"+os.getenv("MONGO_USER")+":"+os.getenv("MONGO_PASSWORD")+"@"+os.getenv("MONGO_HOST")
+    path = "mongodb://"+os.getenv("MONGO_USER")+":"+ os.getenv("MONGO_PASSWORD")+"@"+os.getenv("MONGO_HOST")
     return pymongo.MongoClient(path)
 
-def FindId(id,dbName,collection):
-    client = GetConnection()
+def FindId(client,id,dbName,collection):
     filter={"_id":id}
     with client[dbName][collection].find(filter=filter) as result:
         if(len(list(result.clone()))>0):
             return result[0]
     
 if __name__=="__main__":
-    print(FindId("52ef4b71ab137b00720007d4","mooc","threads"))
+    path = "mongodb://"+os.getenv("MONGO_USER")+":"+ os.getenv("MONGO_PASSWORD")+"@"+os.getenv("MONGO_HOST")
+    print(path)
